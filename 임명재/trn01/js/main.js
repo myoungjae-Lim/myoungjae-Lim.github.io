d1779795fac43bab43a10f542270c9f3648ce860
myoungjae <58833220+myoungjae-Lim@users.noreply.github.com>
2022-05-02 15:14:40 +0900

    Add files via upload

---
+++ 임명재/trn01/js/main.js
@@ -0,0 +1,14 @@
+//document.querySelector('.headerWrap').style.display='none';
+
+//$('.headerWrap').hide();
+
+$(function () {
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        vertical: true,
+        dots:true
+        
+    });
+})
