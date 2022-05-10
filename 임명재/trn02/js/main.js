93dbb928118d7ec171df254117f75d40e301ff09
myoungjae <58833220+myoungjae-Lim@users.noreply.github.com>
2022-05-10 15:16:04 +0900

    Add files via upload

--- 임명재/trn02/js/main.js
+++ 임명재/trn02/js/main.js
@@ -18,6 +18,57 @@ $(function(){
         pauseOnFocus: false,
     });
 
+    $('.movie .dec i:nth-of-type(1)').on('click', function(){
+        $('.movie video').trigger('play');
+    })
+
+    $('.movie .dec i:nth-of-type(2)').on('click', function(){
+        $('.movie video').trigger('pause');
+    })
+
+
+    $('#myMovie').YTPlayer({
+        videoURL:'https://www.youtube.com/watch?v=7xmgRLTjxIw',
+        containment:'.utube',
+        autoPlay:true,
+        mute:true,
+        startAt:0,
+        opacity:1,
+        showControls:false,
+        playOnlyIfVisible:true
+    });
+
+    $('.utube i:nth-of-type(1)').on('click',function(){
+        $('#myMovie').YTPPlay();
+    })
+     
+    $('.utube i:nth-of-type(2)').on('click',function(){
+        $('#myMovie').YTPPause();
+    })
+     
+    $('.product_slide').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+    });
+
+    $('.product i:nth-of-type(1)').on('click',function(){
+        $('.product_slide').slick('slickPrev');
+    });
+
+    $('.product i:nth-of-type(2)').on('click',function(){
+        $('.product_slide').slick('slickNext');
+    });
+
+    $('.product_slide').on('init reInit afterChange', function(){
+        let current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    });
+
+    AOS.init();
+   
+
     // $(window).on('scroll',function(){
     //     let sct = $(window).scrollTop();            
     //     if(sct >= 200){
